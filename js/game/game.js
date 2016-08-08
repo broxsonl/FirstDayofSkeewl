281c597e9e12107a7f8f99da3e6546c49691b44b
ackenepah <ackenepah@gmail.com>
2016-08-08 11:29:12 -0700

    pulling master

--- js/game/game.js
+++ js/game/game.js
@@ -1,1 +1,16 @@
 'use strict';
+// ++++++++++++++++++++++++++++++++++++++++++++
+// DATA - Global Variable declarations
+// ++++++++++++++++++++++++++++++++++++++++++++
+var points = 0;
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+// Global HTML Element Getters
+// ++++++++++++++++++++++++++++++++++++++++++++
+var gameWindow = document.getElementById('game_Window');
+var gameText = document.createElement('p');
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+// Function Declarations
+// ++++++++++++++++++++++++++++++++++++++++++++
