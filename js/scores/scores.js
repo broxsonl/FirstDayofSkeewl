1ae733fb35f63834a4b30cf297e5218887f3aa50
ackenepah <ackenepah@gmail.com>
2016-08-10 17:59:46 -0700

    attempting to animate text in game.html css - saving progress

--- js/scores/scores.js
+++ js/scores/scores.js
@@ -1,31 +1,35 @@
-var table = document.getElementsByTagName('table')[0];
-var tableData = document.getElementsByTagName('tableData')[0];
-var scores;
+var table = document.getElementById('table');
+var tbody = document.getElementById('tbody');
+var scores, rank;
 
 function scoreBoard(){
-  var position = 1;                                   //***
-  for (var i = scores.length ; i > -1; i--) {         //***
+  var position = 1;
+  for (var i = scores.length - 1; i > -1; i--) {
   // for (var i = scores.length; i < ; i++) {
     var tr = document.createElement('tr');
+    var rank = document.createElement('td');
     var user = document.createElement('td');
     var score = document.createElement('td');
     // var prevRound = document.createElement('td');
-    user.innerHTML = scores[i].n;                     //***
-    score.innerHTML = scores[i].s;                    //***
+    rank.innerHTML = position;
+    console.log(scores[i]);
+    user.innerHTML = scores[i].user;
+    score.innerHTML = scores[i].score;
+    tr.appendChild(rank);
     tr.appendChild(user);
     tr.appendChild(score);
-    tableData.appendChild(tr);
-    position++;                                        //***
+    tbody.appendChild(tr);
+    position++;
   }
 }
 
 function scoreLocal() {
-  if (localStorage.scores){
+  if (localStorage.getItem('scores')) {
     scores = JSON.parse(localStorage.scores);
-  }else {
-    scores = [{n:'AAA',s: 1000}, {n: 'ABB', s: 1200}, {n: 'BBB', s: 1400}, {n: 'CCC', s: 1600}, {n: 'DDD', s: 1800}];                                             //***
+  } else {
+    scores = [{user:'Charlie ',score: 500}, {user: 'Caroline ', score: 200}, {user: 'Scotty ', score: 400}, {user: 'Devin ', score: 300}, {user: 'Munir ', score: 500}];
   }
 }
 
-checkLocal();  //***
-makeTable();   //***
+scoreLocal();
+scoreBoard();
